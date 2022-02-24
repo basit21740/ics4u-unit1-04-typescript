3e5a435b9aad115ca12c9b8c4692fa70cb7966ab
Ubuntu <ubuntu@ip-172-31-26-142.ec2.internal>
2022-02-24 17:21:53 +0000

    added

--- src/Dice.ts
+++ src/Dice.ts
@@ -9,31 +9,26 @@
 import promptSync from "prompt-sync"
 const prompt = promptSync()
 
+
+
 var numberOfGuesses = 0
-
-try {
-  const rangeInputString = prompt("Enter a maximum range you would like for the dice (integer): ")
-  const rangeInput = parseFloat(rangeInputString)
-
-  const randomNumber = Math.floor((Math.random() * (rangeInput - 1)) + 1)
+  var randomNumber = Math.floor((Math.random() * 6) + 1)
   const guessInputString = prompt("Guess the number between 1 and your range (integer): ")
   const guessInput = parseInt(guessInputString, 10)
-  
-   numberOfGuesses = numberOfGuesses + 1;
+  numberOfGuesses = numberOfGuesses + 1;
+ 
+   do {
+    const guessInputString = prompt("Guess the number between 1 and your range (integer): ")
+    const guessInput = parseInt(guessInputString)
 
-        if (isNaN(guessInput) == true) {
-          console.log("ERROR: Invalid Input");
-          console.log();
-        } else if (guessInput == randomNumber) {
-          // Output if user guesses number correctly
-          console.log("You guessed the correct number!");
-          console.log("It took you", numberOfGuesses, "guesses.");
-          console.log();
-        } else {
-          // Output if user guesses number incorrectly
-          console.log("Wrong number, try again!");
-          console.log();
-}
-}finally {
-  ("")
-}
+  if (guessInput > randomNumber) {
+    console.log("Wrong , You guessed too high, think smaller");
+    console.log("Guess again")
+  } else if (guessInput < randomNumber) {
+    console.log("Wrong , You guessed too low, think bigger");
+    console.log("Guess again")
+  } else {
+    console.log("You guessed the right number!")
+    console.log("It took you", numberOfGuesses, "guesses.")
+  }
+} while (guessInput != randomNumber);
